76476b4e646be515a63ade0f91da409c7af74afb
SergeyLeonovich <112343906+SergeyLeonovich@users.noreply.github.com>
2024-07-21 19:56:09 +0200

    Update Program.cs

--- GeniyIdiotConsoleApp/Program.cs
+++ GeniyIdiotConsoleApp/Program.cs
@@ -10,7 +10,7 @@ namespace GeniyIdiotConsoleApp
     {
         
 
-
+    //Commint
         static void Main(string[] args)
         {
             Console.WriteLine("Привет дружище!Напиши, как тебя зовут?");
